d56103c975278ac83d0a070b5924d834b8ced5d2
ByungDae, Sohn <miconblog@gmail.com>
2014-02-23 16:12:31 +0900

    백본 모델의 기본 SYNC 과정과 오버라이딩

--- 20140224/public/js/main.js
+++ 20140224/public/js/main.js
@@ -10,6 +10,16 @@ window.app = {
         'use strict';
         
         var model = new app.Models.UserModel();
+
+        model.on("request", function(model, xhr, options){
+            console.log("### REQUEST from app's model\n", model, xhr, options);
+        });
+
+        model.on("sync", function(model, response, options){
+
+            console.log("### SYNC from app's model\n", model, response, options);
+
+        });
         model.fetch();
         console.log("--- main application init --- ");
     }
--- 20140224/public/js/models/user.js
+++ 20140224/public/js/models/user.js
@@ -15,6 +15,23 @@ app.Models = app.Models || {};
       console.log("urlRoot: ", this.urlRoot)
       return this.urlRoot;
     },
+    parse : function(response, options){
+      console.log("===> parse of model ", response, options)
+    },
+    /**
+     * sync 함수가 정의되면, Backbone.sync 의 기본 동작을 덮어 쓴다.
+     * 
+     *  - 백본 sync의 기본 동작은 url이 있을 경우 해당 url로 요청을 날린다.  
+     *
+     * method – the CRUD method ("create", "read", "update", or "delete")
+     * model – the model to be saved (or collection to be read)
+     * options – success and error callbacks, and all other jQuery request option
+     */
+    // sync : function(method, model, options){
+
+    //   console.log("===> sync of model\n", method, model, options);
+    //   options.success && options.success([1,2,3,4]);
+    // },
 
     initialize: function(){
       console.log("---- UserModel init ---\n", this.toJSON());
